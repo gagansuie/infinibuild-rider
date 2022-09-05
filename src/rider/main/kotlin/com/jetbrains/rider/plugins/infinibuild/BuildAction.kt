69972f648795e96c5f1b71ac5259415a63c6b9be
Gagan Suie <gagan.suie@codecrow.io>
2022-09-04 19:34:51 -0500

    Added client code to upload files

--- src/rider/main/kotlin/com/jetbrains/rider/plugins/infinibuild/BuildAction.kt
+++ src/rider/main/kotlin/com/jetbrains/rider/plugins/infinibuild/BuildAction.kt
@@ -3,9 +3,28 @@ package com.jetbrains.rider.plugins.infinibuild
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.ui.Messages
+import kotlinx.coroutines.*
+import org.http4k.client.OkHttp
+import org.http4k.core.Method
+import org.http4k.core.Request
 
 class BuildAction() : AnAction() {
+
+    @OptIn(DelicateCoroutinesApi::class)
     override fun actionPerformed(p0: AnActionEvent) {
-        Messages.showMessageDialog(p0.project, "Hello", "infinibuild", Messages.getInformationIcon())
+        GlobalScope.launch(Dispatchers.Main) {
+            val client = OkHttp()
+            val response = client(Request(Method.GET, Constants.BASE_URL))
+//            withContext(Dispatchers.IO) {
+//                Thread.sleep(5000)
+//            }
+//            Utils.uploadFiles(Constants.BASE_URL, arrayOf())
+            Messages.showMessageDialog(
+                p0.project,
+                response.bodyString(),
+                "Response",
+                Messages.getInformationIcon()
+            )
+        }
     }
 }
